e66bcadbee49051b4103c806c64fb0a42ef34530
Gus <33813160+Chemchu@users.noreply.github.com>
2023-01-24 13:52:56 +0100

    Lualine actualizado

--- init.lua
+++ init.lua
@@ -325,6 +325,15 @@ require('lualine').setup {
     component_separators = '|',
     section_separators = '',
   },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 1
+      }
+    }
+  }
 }
 
 -- Enable Comment.nvim
