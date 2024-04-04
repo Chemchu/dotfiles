63a6bd01e403c631dc7392f65be5c92bd0133990
gus <gustavo_9_rj@hotmail.com>
2024-04-05 01:09:47 +0200

    feat: added mute/increase/decrease volume

--- home/programs/waybar/default.nix
+++ home/programs/waybar/default.nix
@@ -6,8 +6,8 @@
     package = pkgs.waybar;   
   };
 
-  home.file.".config/waybar/style.css".source = ./style.css;
-  home.file.".config/waybar/config".source = ./config;
-  home.file.".config/waybar/scripts".source = ./scripts;
-  home.file.".config/waybar/scripts".recursive = true;
+  home.file."~/.config/waybar/style.css".source = ./style.css;
+  home.file."~/.config/waybar/config".source = ./config;
+  home.file."~/.config/waybar/scripts".source = ./scripts;
+  home.file."~/.config/waybar/scripts".recursive = true;
 }
