8e46ebb1a9b80c73eba2f125bf92c090cca4f85b
gus <gustavo_9_rj@hotmail.com>
2024-04-07 02:54:48 +0200

    fix: fixed workspace spacing

--- home/programs/waybar/default.nix
+++ home/programs/waybar/default.nix
@@ -1,11 +1,6 @@
 {pkgs, ...}:
 {
 
-  #programs.waybar = {
-  #  enable = true;
-  #  package = pkgs.waybar;   
-  #};
-
   home.packages = with pkgs; [
     font-awesome # --> It's here because it's the closest to weather.py which was not able to render emojis
     playerctl
