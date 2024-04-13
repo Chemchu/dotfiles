14f54df0078b375e87141805ed84575331253dfc
gus <gustavo_9_rj@hotmail.com>
2024-04-13 20:41:48 +0200

    test: testing things

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nixvim, ... }@inputs:
 	let 
 		system = "x86_64-linux"; 
 		pkgs = nixpkgs.legacyPackages.${system};
--- home/programs/nixvim/default.nix
+++ home/programs/nixvim/default.nix
@@ -3,5 +3,20 @@
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
   ];
-  programs.nixvim.enable = true;
+
+  home.shellAliases.v = "nvim";
+ 
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+
+    viAlias = true;
+    vimAlias = true;
+
+    # luaLoader.enable = true;
+
+    # Highlight and remove extra white spaces
+    #highlight.ExtraWhitespace.bg = "red";
+    #match.ExtraWhitespace = "\\s\\+$";
+  };
 }
