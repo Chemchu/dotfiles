709c75f5a24d0d06735da0a8cdd63d881c8ba83d
gus <gustavo_9_rj@hotmail.com>
2024-04-25 23:27:03 +0200

    fix: direnv by home-manager

--- home/commons/programs/dev/direnv.nix
+++ home/commons/programs/dev/direnv.nix
@@ -1,11 +1,11 @@
 {
-  pkgs,
-  ...
-}: {
-  home = {
-    packages = with pkgs; [
-      direnv
-    ];
+  programs = {
+    direnv = {
+      enable = true;
+      enableZshIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+
+    zsh.enable = true; # see note on other shells below
   };
 }
-
