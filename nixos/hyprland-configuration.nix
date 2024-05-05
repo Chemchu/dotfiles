d72099134fd004d49670bd4db09cddd4b7322b46
Chemchu <gustavo_9_rj@hotmail.com>
2024-05-05 20:11:36 +0200

    feat: new nvidia driver version

--- nixos/hyprland-configuration.nix
+++ nixos/hyprland-configuration.nix
@@ -134,16 +134,17 @@
 
       open = false;
 
-      #package = config.boot.kernelPackages.nvidiaPackages.stable; # ---> This does not currently work for me
+      package = config.boot.kernelPackages.nvidiaPackages.stable; # ---> This does not currently work for me
+
       # I had to use this specific driver because versions 550 just breaks everything
-      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-         version = "535.129.03";
-         sha256_64bit = "sha256-5tylYmomCMa7KgRs/LfBrzOLnpYafdkKwJu4oSb/AC4=";
-         sha256_aarch64 = "sha256-i6jZYUV6JBvN+Rt21v4vNstHPIu9sC+2ZQpiLOLoWzM=";
-         openSha256 = "sha256-/Hxod/LQ4CGZN1B1GRpgE/xgoYlkPpMh+n8L7tmxwjs=";
-         settingsSha256 = "sha256-QKN/gLGlT+/hAdYKlkIjZTgvubzQTt4/ki5Y+2Zj3pk=";
-         persistencedSha256 = "sha256-FRMqY5uAJzq3o+YdM2Mdjj8Df6/cuUUAnh52Ne4koME=";
-      };
+      #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      #   version = "535.129.03";
+      #   sha256_64bit = "sha256-5tylYmomCMa7KgRs/LfBrzOLnpYafdkKwJu4oSb/AC4=";
+      #   sha256_aarch64 = "sha256-i6jZYUV6JBvN+Rt21v4vNstHPIu9sC+2ZQpiLOLoWzM=";
+      #   openSha256 = "sha256-/Hxod/LQ4CGZN1B1GRpgE/xgoYlkPpMh+n8L7tmxwjs=";
+      #   settingsSha256 = "sha256-QKN/gLGlT+/hAdYKlkIjZTgvubzQTt4/ki5Y+2Zj3pk=";
+      #   persistencedSha256 = "sha256-FRMqY5uAJzq3o+YdM2Mdjj8Df6/cuUUAnh52Ne4koME=";
+      #};
 
     };
   };
