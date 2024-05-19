f81596720bf34875cf042202576c29d964aff1f7
Gustavo Lee <glee.sopra@grupogimeno.onmicrosoft.com>
2024-05-19 19:14:00 +0200

    feat: added jdtls and dap but I don't actually know how to properly set it up

--- home/commons/programs/dev/java.nix
+++ home/commons/programs/dev/java.nix
@@ -1,4 +1,29 @@
+{ pkgs, lib, ... }:
 {
+  home = {
+    packages = with pkgs; [
+      # Packages needed for Java
+      temurin-bin-17
+    ];
+  };
+
+  programs.nixvim = {
+    plugins.nvim-jdtls = {
+      enable = true;
+      cmd = [
+        (lib.getExe pkgs.jdt-language-server)
+      ];
+    };
+
+    plugins.lsp = {
+      enable = true;
+      servers = {
+        java-language-server = { enable = true; };
+      };
+    };
+  };
+}
+/* {
   pkgs,
   ...
 }: {
@@ -10,13 +35,51 @@
   };
 
   programs.nixvim = {
+    enable = true;
+
     plugins = {
-      lsp.servers = {
-        java-language-server = {
-          enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          jdtls = {
+            enable = true;
+            lspConfig = {
+              name = "jdtls";
+              cmd = "${pkgs.jdtls}/bin/jdtls";
+              filetypes = ["java"];
+            };
+          };
+        };
+      };
+
+      dap = {
+        enable = true;
+        config = {
+          java = {
+            adapters = {
+              javaDebug = {
+                type = "executable";
+                command = "java";
+                args = [
+                  "-jar"
+                  "${pkgs.vscode-java-debug}/share/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
+                ];
+              };
+            };
+
+            configurations = [
+              {
+                type = "java";
+                name = "Debug (Launch) - Current File";
+                request = "launch";
+                #mainClass = "${file}";
+                #projectName = "${workspaceFolderBasename}";
+              }
+            ];
+          };
         };
       };
     };
   };
 }
-
+*/
