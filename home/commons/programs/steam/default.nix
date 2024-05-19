28d20fa3969a8a1431325852d28e20992f40f719
Chemchu <gustavo_9_rj@hotmail.com>
2024-05-19 22:49:57 +0200

    feat: added steam correctly

--- home/commons/programs/steam/default.nix
+++ home/commons/programs/steam/default.nix
@@ -1,11 +1,7 @@
+{ pkgs, ... }:
 {
-  programs.steam = {
-    enable = true;
-
-    # Open ports in the firewall for Steam Remote Play
-    remotePlay.openFirewall = true;
-
-    # Open ports in the firewall for Source Dedicated Server
-    dedicatedServer.openFirewall = true;
-  };
+  home.packages = [
+    # Other packages...
+    pkgs.steam
+  ];
 }
