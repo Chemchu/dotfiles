5fe6c3fd3aecd771613d6d200e85fc622830c4c9
Chemchu <gustavo_9_rj@hotmail.com>
2024-05-19 15:02:57 +0200

    fix: testing

--- home/hyprland/hyprland-commons/hypr/default.nix
+++ home/hyprland/hyprland-commons/hypr/default.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 let
-  wallpaper_path = ".config/hypr/wallpapers";
   wallpaper = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/dharmx/walls/main/flowers/a_painting_of_flowers_in_a_vase.jpeg";
     # replace this with the SHA256 hash of the image file
@@ -19,7 +18,6 @@ in
   ];
 
   # Here I import all my wallpapers
-  home.file."${wallpaper_path}/wallpaper".source = wallpaper;
   home.file.".local/share/icons".source = ../../../icons;
   home.file.".local/share/icons".recursive = true;
 
@@ -47,7 +45,8 @@ in
       # See https://wiki.hyprland.org/Configuring/Keywords/ for more
 
       # Execute your favorite apps at launch
-      exec-once = swww img ~/${wallpaper_path}/wallpaper
+      exec-once = swww init
+      exec-once = swww img ${wallpaper}
       exec-once = waybar
       exec-once = dunst
 
