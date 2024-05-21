152b4d2fb9c373587a631cdda21507c6a33e4a3a
Gustavo Lee <glee.sopra@grupogimeno.onmicrosoft.com>
2024-05-22 00:29:04 +0200

    feat: added intellij to java dev

--- home/commons/programs/dev/java.nix
+++ home/commons/programs/dev/java.nix
@@ -1,85 +1,30 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
-  /* home = {
-    packages = with pkgs; [
-      # Packages needed for Java
-      temurin-bin-17
-    ];
-  }; */
-
-  programs.nixvim = {
-    plugins.nvim-jdtls = {
-      enable = true;
-      cmd = [
-        (lib.getExe pkgs.jdt-language-server)
-      ];
-    };
-
-    plugins.lsp = {
-      enable = true;
-      servers = {
-        java-language-server = { enable = true; };
-      };
-    };
-  };
-}
-/* {
-  pkgs,
-  ...
-}: {
   home = {
     packages = with pkgs; [
       # Packages needed for Java
       temurin-bin-17
+      #jdk
+      jdt-language-server
+      jetbrains.idea-community
     ];
   };
 
   programs.nixvim = {
-    enable = true;
-
     plugins = {
+      nvim-jdtls = {
+        enable = true;
+        cmd = [
+          "${pkgs.jdt-language-server}/bin/jdt-language-server"
+        ];
+      };
+
       lsp = {
         enable = true;
         servers = {
-          jdtls = {
-            enable = true;
-            lspConfig = {
-              name = "jdtls";
-              cmd = "${pkgs.jdtls}/bin/jdtls";
-              filetypes = ["java"];
-            };
-          };
-        };
-      };
-
-      dap = {
-        enable = true;
-        config = {
-          java = {
-            adapters = {
-              javaDebug = {
-                type = "executable";
-                command = "java";
-                args = [
-                  "-jar"
-                  "${pkgs.vscode-java-debug}/share/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
-                ];
-              };
-            };
-
-            configurations = [
-              {
-                type = "java";
-                name = "Debug (Launch) - Current File";
-                request = "launch";
-                #mainClass = "${file}";
-                #projectName = "${workspaceFolderBasename}";
-              }
-            ];
-          };
+          java-language-server = { enable = true; };
         };
       };
     };
   };
 }
-*/
