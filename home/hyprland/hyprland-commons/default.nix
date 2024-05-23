8157f5de4b8f2b48ee46f4c7b0eb4d418a419421
Chemchu <gustavo_9_rj@hotmail.com>
2024-05-24 00:50:14 +0200

    feat: new cursor?

--- home/hyprland/hyprland-commons/default.nix
+++ home/hyprland/hyprland-commons/default.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   ...
 }: {
   imports = [
@@ -6,4 +7,12 @@
     ./rofi
     ./waybar
   ];
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Amber";
+    size = 28;
+  };
 }
