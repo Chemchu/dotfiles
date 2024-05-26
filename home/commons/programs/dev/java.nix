0b8aa8dfb4f06f963a2864610500e4f589d56a65
Gustavo Lee <glee.sopra@grupogimeno.onmicrosoft.com>
2024-05-26 18:10:45 +0200

    feat: trying jdtls and java-language-server. Nothing works, java sucksç

--- home/commons/programs/dev/java.nix
+++ home/commons/programs/dev/java.nix
@@ -1,22 +1,32 @@
-{ pkgs, ... }:
 {
+  pkgs,
+  config,
+  ...
+}: {
   home = {
     packages = with pkgs; [
       # Packages needed for Java
       jdk17
       jdt-language-server
       jetbrains.idea-community
-      vimPlugins.nvim-jdtls
+      lombok
     ];
   };
 
   programs.nixvim = {
     plugins = {
       nvim-jdtls = {
-        enable = true;
+        enable = false;
+        data = "${config.xdg.cacheHome}/jdtls/workspace";
+        configuration = "${config.xdg.cacheHome}/jdtls/config";
+        initOptions = null;
+        rootDir = {
+          __raw = "require('jdtls.setup').find_root({'.git','mvnw','pom.xml', 'gradlew'})";
+        };
+        settings = null;
         cmd = [
-          "${pkgs.jdt-language-server}/bin/jdt-language-server"
-          "-data ${pkgs.jdt-language-server}/workspace"
+          "${pkgs.jdt-language-server}/bin/jdtls"
+          "-settings /home/gus/ssg/facsa/waternology/tools/apache-maven-3.8.4/conf/settings.xml"
         ];
       };
 
