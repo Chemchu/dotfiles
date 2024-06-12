18ebc9b2bf0be61d0707e4debc2ec98cfd31c38a
Chemchu <gustavo_9_rj@hotmail.com>
2024-06-12 21:25:08 +0200

    feat: please work

--- home/commons/programs/dev/typescript.nix
+++ home/commons/programs/dev/typescript.nix
@@ -3,13 +3,60 @@
 }: {
   programs.nixvim = {
     plugins = {
+      typescript-tools = {
+        enable = true;
+        onAttach = ''
+          function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+
+            if vim.lsp.inlay_hint then
+              vim.lsp.inlay_hint(bufnr, true)
+            end
+          end
+        '';
+        settings = {
+          tsserverFilePreferences = {
+            # Inlay Hints
+            includeInlayParameterNameHints = "all";
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+            includeInlayFunctionParameterTypeHints = true;
+            includeInlayVariableTypeHints = true;
+            includeInlayVariableTypeHintsWhenTypeMatchesName = true;
+            includeInlayPropertyDeclarationTypeHints = true;
+            includeInlayFunctionLikeReturnTypeHints = true;
+            includeInlayEnumMemberValueHints = true;
+          };
+        };
+      };
       lsp.servers = {
         tsserver = {
-          enable = true;
-          settings = {
-            tsserver = {
-              format = {
-                enable = true;
+          enable = false;
+          autostart = true;
+          filetypes = ["javascript" "javascriptreact" "typescript" "typescriptreact"];
+          extraOptions = {
+            settings = {
+              javascript = {
+                inlayHints = {
+                  includeInlayEnumMemberValueHints = true;
+                  includeInlayFunctionLikeReturnTypeHints = true;
+                  includeInlayFunctionParameterTypeHints = true;
+                  includeInlayParameterNameHints = "all";
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+                  includeInlayPropertyDeclarationTypeHints = true;
+                  includeInlayVariableTypeHints = true;
+                };
+              };
+              typescript = {
+                inlayHints = {
+                  includeInlayEnumMemberValueHints = true;
+                  includeInlayFunctionLikeReturnTypeHints = true;
+                  includeInlayFunctionParameterTypeHints = true;
+                  includeInlayParameterNameHints = "all";
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+                  includeInlayPropertyDeclarationTypeHints = true;
+                  includeInlayVariableTypeHints = true;
+                };
               };
             };
           };
