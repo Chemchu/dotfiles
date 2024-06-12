4c97b5d36d29fc0228548766bf95b5966821d595
Chemchu <gustavo_9_rj@hotmail.com>
2024-06-12 22:42:54 +0200

    feat(WIP): ags config being done

--- home/hyprland/hyprland-commons/ags/ags-config/main.ts
+++ home/hyprland/hyprland-commons/ags/ags-config/main.ts
@@ -1,26 +1,8 @@
-const time = Variable('', {
-    poll: [1000, function() {
-        return Date().toString()
-    }],
-})
-
-const Bar = (monitor: number) => Widget.Window({
-    monitor,
-    name: `bar${monitor}`,
-    anchor: ['top', 'left', 'right'],
-    exclusivity: 'exclusive',
-    child: Widget.CenterBox({
-        start_widget: Widget.Label({
-            hpack: 'center',
-            label: 'Welcome to AGS!',
-        }),
-        end_widget: Widget.Label({
-            hpack: 'center',
-            label: time.bind(),
-        }),
-    }),
-})
+import { Bar } from "src/bar/main"
 
 App.config({
-    windows: [Bar(0)],
+    style: "./style.css",
+    windows: [
+        Bar(),
+    ],
 })
