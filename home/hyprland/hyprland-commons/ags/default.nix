a19b3836c772afe795cac159530222dc7e5adbcf
Chemchu <gustavo_9_rj@hotmail.com>
2024-06-14 01:22:50 +0200

    feat: changed to ags

--- home/hyprland/hyprland-commons/ags/default.nix
+++ home/hyprland/hyprland-commons/ags/default.nix
@@ -1,7 +1,11 @@
-{ inputs, pkgs, ... }:
 {
-  # add the home manager module
-  imports = [ inputs.ags.homeManagerModules.default ];
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    inputs.ags.homeManagerModules.default
+  ];
 
   home.packages = with pkgs; [
     dart-sass
@@ -18,24 +22,11 @@
     gtk3
   ];
 
-  programs = {
-    ags = {
-      enable = true;
-
-      # null or path, leave as null if you don't want hm to manage the config
-      configDir = null;
-
-      # additional packages to add to gjs's runtime
-      extraPackages = with pkgs; [
-        gtksourceview
-        webkitgtk
-        accountsservice
-      ];
-    };
+  programs.ags = {
+    enable = true;
+    configDir = ../ags;
+    extraPackages = with pkgs; [
+      accountsservice
+    ];
   };
-
-  # AGS configuration project
-  home.file.".config/ags".source = ./ags-config;
-  home.file.".config/ags".recursive = true;
-
 }
