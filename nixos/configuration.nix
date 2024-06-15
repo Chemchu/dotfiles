9b773d78565e8f1e29d883b515c2429064faf59c
Chemchu <gustavo_9_rj@hotmail.com>
2024-06-15 21:38:57 +0200

    feat: simplified right menu

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -71,6 +71,21 @@
     };
   };
 
+  services = {
+    gvfs.enable = true;
+    devmon.enable = true;
+    udisks2.enable = true;
+    upower.enable = true;
+    power-profiles-daemon.enable = true;
+    accounts-daemon.enable = true;
+    gnome = {
+      evolution-data-server.enable = true;
+      glib-networking.enable = true;
+      gnome-keyring.enable = true;
+      gnome-online-accounts.enable = true;
+    };
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
