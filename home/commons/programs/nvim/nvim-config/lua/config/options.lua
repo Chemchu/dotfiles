2f497dd0d01f6ffd2cda0b249b718cac4b0a8b9f
Chemchu <gustavo_9_rj@hotmail.com>
2024-06-21 00:29:45 +0200

    feat: added lazyvim to config

--- home/commons/programs/nvim/nvim-config/lua/config/options.lua
+++ home/commons/programs/nvim/nvim-config/lua/config/options.lua
@@ -1,22 +1,3 @@
-local opt = vim.opt
-
-opt.wrap = true
-opt.conceallevel = 1
-opt.cursorline = false
-opt.number = true -- Print line number
-opt.relativenumber = true -- Relative line numbers
-opt.hlsearch = false -- highlight search
-opt.incsearch = true -- incremental search
-opt.scrolloff = 4 -- scroll offset
-opt.clipboard = "unnamedplus" -- sync clipboard with os
-opt.breakindent = true
-opt.inccommand = "split"
-
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-
-opt.swapfile = false
-
-opt.cinoptions:append(":0") -- switch statement indentations
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
