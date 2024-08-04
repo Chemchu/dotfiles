880da986e447dcf76f8f64e51a8b68128e618680
Chemchu <gustavo_9_rj@hotmail.com>
2024-08-04 19:31:20 +0200

    fix: scripts references removed

--- home/hyprland/hyprland-commons/waybar/default.nix
+++ home/hyprland/hyprland-commons/waybar/default.nix
@@ -9,8 +9,6 @@
     playerctl
   ];
 
-  home.file.".config/waybar/scripts".recursive = true;
-  home.file.".config/waybar/scripts".source = ./scripts;
   home.file.".config/waybar/config".source = ./config;
   home.file.".config/waybar/style.css".source = ./style.css;
 }
