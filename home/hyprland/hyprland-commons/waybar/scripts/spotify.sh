e0702fd9eb9a7ddab6f5652ba95be145d8640696
Chemchu <gustavo_9_rj@hotmail.com>
2024-08-04 18:09:53 +0200

    feat: added spotify color only to logo

--- home/hyprland/hyprland-commons/waybar/scripts/spotify.sh
+++ home/hyprland/hyprland-commons/waybar/scripts/spotify.sh
@@ -1,27 +1,41 @@
 #!/bin/sh
 
+truncate() {
+    local input="$1"
+    local length="$2"
+    if [ ${#input} -gt $length ]; then
+        echo "${input:0:$length}..."
+    else
+        echo "$input"
+    fi
+}
+
 while true; do
 
-	player_status=$(playerctl status 2>/dev/null)
+    player_status=$(playerctl status 2>/dev/null)
+    artist=$(playerctl metadata artist)
+    title=$(playerctl metadata title)
+    spotify_display_text="$artist - $title"
+    spotify_display_text=$(truncate "$spotify_display_text" 30)  # Truncate the title if it exceeds 30 characters
 
-	if [ -z "$(playerctl metadata album)" ]; then
-		if [ "$player_status" = "Playing" ]; then
-			echo "$(playerctl metadata artist) - $(playerctl metadata title)"
-		elif [ "$player_status" = "Paused" ]; then
-			echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    if [ -z "$(playerctl metadata album)" ]; then
+        if [ "$player_status" = "Playing" ]; then
+            echo "$spotify_display_text"
+        elif [ "$player_status" = "Paused" ]; then
+            echo " $spotify_display_text"
+        else
+            echo ""
+        fi
     else
-			echo ""
-		fi
-	else
-		if [ "$player_status" = "Playing" ]; then
-			echo "<span color='#1db954'></span> $(playerctl metadata artist) - $(playerctl metadata title)"
-		elif [ "$player_status" = "Paused" ]; then
-			echo "<span color='#1db954'></span>  $(playerctl metadata artist) - $(playerctl metadata title)"
-    else
-			echo ""
-		fi
-	fi
+        if [ "$player_status" = "Playing" ]; then
+            echo "<span class='logo'></span> $spotify_display_text"
+        elif [ "$player_status" = "Paused" ]; then
+            echo "<span class='logo'></span>  $spotify_display_text"
+        else
+            echo ""
+        fi
+    fi
 
-	sleep 1
+    sleep 1
 
 done
