9f13e075c09371b4c2112f50074498f9cbb5e489
Chemchu <gustavo_9_rj@hotmail.com>
2024-08-22 00:42:34 +0200

    fix: dbeaver flickering

--- nixos/configuration-components/ld.nix
+++ nixos/configuration-components/ld.nix
@@ -46,25 +46,4 @@
     qt5.qtwayland
     stdenv.cc.cc
   ];
-
-  # Additional environment variables
-  #environment.variables = {
-  #  NIX_LD_LIBRARY_PATH = builtins.concatStringsSep ":" (map (lib: lib.dev)/usr/lib) [
-  #    pkgs.nss
-  #    pkgs.sane-backends
-  #    pkgs.nspr
-  #    pkgs.zlib
-  #    pkgs.libglvnd
-  #    pkgs.qt5.qtbase
-  #    pkgs.qt5.qtsvg
-  #    pkgs.qt5.qtsvg
-  #    pkgs.qt5.qtdeclarative
-  #    pkgs.qt5.qtwayland
-  #    pkgs.stdenv.cc.cc
-  #  ];
-
-  #  #NIX_LD = builtins.readFile "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
-  #  QT_PLUGIN_PATH = "${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.qtPluginPrefix}:${pkgs.qt5.qtwayland.bin}/${pkgs.qt5.qtbase.qtPluginPrefix}";
-  #  QML2_IMPORT_PATH = "${pkgs.qt5.qtdeclarative.bin}/${pkgs.qt5.qtbase.qtQmlPrefix}:${pkgs.qt5.qtwayland.bin}/${pkgs.qt5.qtbase.qtQmlPrefix}";
-  #};
 }
