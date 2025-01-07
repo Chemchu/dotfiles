c1f77c4775dac3ddc0c567b6257824f6127b1f10
Chemchu <gustavo_9_rj@hotmail.com>
2025-01-07 21:43:01 +0100

    feat: nvf (neovim) config done by now

--- home/commons/programs/nvf/default.nix
+++ home/commons/programs/nvf/default.nix
@@ -1,8 +1,4 @@
-{
-  inputs,
-  ...
-}:
-{
+{inputs, ...}: {
   imports = [
     inputs.nvf.homeManagerModules.default
   ];
@@ -14,50 +10,277 @@
       vim = {
         viAlias = true;
         vimAlias = true;
+        debugMode = {
+          enable = false;
+          level = 16;
+          logFile = "/tmp/nvim.log";
+        };
+
+        spellcheck = {
+          enable = false;
+        };
+
+        lsp = {
+          formatOnSave = true;
+          lspkind.enable = false;
+          lightbulb.enable = true;
+          lspsaga.enable = false;
+          trouble.enable = true;
+          lspSignature.enable = true;
+          otter-nvim.enable = true;
+          lsplines.enable = true;
+          nvim-docs-view.enable = true;
+        };
+
+        debugger = {
+          nvim-dap = {
+            enable = true;
+            ui.enable = true;
+          };
+        };
+
+        # This section does not include a comprehensive list of available language modules.
+        # To list all available language module options, please visit the nvf manual.
+        languages = {
+          enableLSP = true;
+          enableFormat = true;
+          enableTreesitter = true;
+          enableExtraDiagnostics = true;
+
+          # Languages that will be supported in default and maximal configurations.
+          nix.enable = true;
+          markdown.enable = true;
+
+          # Languages that are enabled in the maximal configuration.
+          bash.enable = true;
+          clang.enable = true;
+          css.enable = true;
+          html.enable = true;
+          sql.enable = true;
+          java.enable = true;
+          kotlin.enable = false;
+          ts.enable = true;
+          go.enable = true;
+          lua.enable = true;
+          zig.enable = true;
+          python.enable = true;
+          typst.enable = false;
+          rust = {
+            enable = true;
+            crates.enable = true;
+          };
+
+          # Language modules that are not as common.
+          assembly.enable = false;
+          astro.enable = false;
+          nu.enable = false;
+          csharp.enable = false;
+          julia.enable = false;
+          vala.enable = false;
+          scala.enable = false;
+          r.enable = false;
+          gleam.enable = false;
+          dart.enable = false;
+          ocaml.enable = false;
+          elixir.enable = false;
+          haskell.enable = false;
+
+          tailwind.enable = false;
+          svelte.enable = false;
+
+          # Nim LSP is broken on Darwin and therefore
+          # should be disabled by default. Users may still enable
+          # `vim.languages.vim` to enable it, this does not restrict
+          # that.
+          # See: <https://github.com/PMunch/nimlsp/issues/178#issue-2128106096>
+          nim.enable = false;
+        };
+
+        visuals = {
+          nvim-scrollbar.enable = true;
+          nvim-web-devicons.enable = true;
+          nvim-cursorline.enable = true;
+          cinnamon-nvim.enable = true;
+          fidget-nvim.enable = true;
+
+          highlight-undo.enable = true;
+          indent-blankline.enable = true;
+
+          # Fun
+          cellular-automaton.enable = false;
+        };
+
+        statusline = {
+          lualine = {
+            enable = true;
+            theme = "onedark";
+          };
+        };
+
         theme = {
           enable = true;
           name = "onedark";
+          style = "darker";
+          transparent = false;
         };
-        statusline.lualine.enable = true;
-        tabline.nvimBufferline.enable = true;
+
         autopairs.nvim-autopairs.enable = true;
-        telescope.enable = true;
-        autocomplete.nvim-cmp.enable = true;
-        utility.motion.hop.enable = true;
-        ui.colorizer.enable = true;
-        comments.comment-nvim.enable = true;
-        debugger.nvim-dap.enable = true;
-        utility.preview.glow.enable = true;
-        lsp.trouble.enable = true;
-        filetree.neo-tree.enable = true;
-        terminal.toggleterm.enable = true;
 
-        languages = {
-          markdown.enable = true;
-          enableLSP = true;
-          enableTreesitter = true;
-          nix.enable = true;
-          ts.enable = true;
-          rust.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+        snippets.luasnip.enable = true;
+
+        filetree = {
+          neo-tree = {
+            enable = false;
+          };
+          nvimTree = {
+            enable = true;
+            mappings.toggle = "<leader>e";
+          };
         };
 
-        binds.whichKey = {
+        tabline = {
+          nvimBufferline.enable = true;
+        };
+
+        treesitter.context.enable = true;
+
+        binds = {
+          whichKey.enable = true;
+          cheatsheet.enable = true;
+        };
+
+        telescope.enable = true;
+
+        git = {
           enable = true;
+          gitsigns.enable = false;
+          gitsigns.codeActions.enable = false; # throws an annoying debug message
+        };
+
+        minimap = {
+          minimap-vim.enable = false;
+          codewindow.enable = false; # lighter, faster, and uses lua for configuration
+        };
+
+        dashboard = {
+          dashboard-nvim.enable = false;
+          alpha.enable = true;
+        };
+
+        notify = {
+          nvim-notify.enable = true;
+        };
+
+        projects = {
+          project-nvim.enable = true;
+        };
+
+        utility = {
+          ccc.enable = false;
+          vim-wakatime.enable = false;
+          surround.enable = true;
+          diffview-nvim.enable = true;
+          motion = {
+            hop.enable = false;
+            leap.enable = true;
+            precognition.enable = false;
+          };
+
+          images = {
+            image-nvim.enable = false;
+          };
+        };
+
+        notes = {
+          obsidian.enable = false;
+          neorg.enable = false;
+          orgmode.enable = false;
+          mind-nvim.enable = false;
+          todo-comments.enable = true;
+        };
+
+        terminal = {
+          toggleterm = {
+            enable = true;
+            lazygit.enable = false;
+          };
+        };
+
+        ui = {
+          borders.enable = true;
+          noice.enable = true;
+          colorizer.enable = true;
+          modes-nvim.enable = false; # the theme looks terrible with catppuccin
+          illuminate.enable = true;
+          breadcrumbs = {
+            enable = false;
+            navbuddy.enable = false;
+          };
+          smartcolumn = {
+            enable = true;
+            setupOpts.custom_colorcolumn = {
+              # this is a freeform module, it's `buftype = int;` for configuring column position
+              nix = "110";
+              ruby = "120";
+              java = "130";
+              go = ["90" "130"];
+            };
+          };
+          fastaction.enable = true;
+        };
+
+        assistant = {
+          chatgpt.enable = false;
+          copilot = {
+            enable = false;
+          };
+        };
+
+        session = {
+          nvim-session-manager.enable = false;
+        };
+
+        gestures = {
+          gesture-nvim.enable = false;
+        };
+
+        comments = {
+          comment-nvim.enable = true;
+        };
+
+        presence = {
+          neocord.enable = false;
         };
 
         keymaps = [
           {
-            key = "<Space>";
-            mode = ["n" "v" "c"];
-            silent = true;
-            action = "<NOP>";
-          }
-          {
-            key = "<leader>s";
+            key = "<leader>w";
             mode = "n";
             silent = true;
             action = ":w<CR>";
-            desc = "Save buffer";
+            desc = "Save";
+          }
+          {
+            key = "<leader>bx";
+            mode = "n";
+            silent = true;
+            action = ":bd<CR>";
+            desc = "Close buffer";
+          }
+          {
+            key = "<TAB>";
+            mode = "n";
+            silent = true;
+            action = ":bnext<CR>";
+            desc = "Next buffer";
+          }
+          {
+            key = "<S-TAB>";
+            mode = "n";
+            silent = true;
+            action = ":bprevious<CR>";
+            desc = "Previous buffer";
           }
         ];
       };
