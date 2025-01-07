c417e16c968cbb4f57cf86d96f9ae1b79938cc5b
Chemchu <gustavo_9_rj@hotmail.com>
2025-01-07 20:58:24 +0100

    feat: some neovim config

--- home/commons/programs/nvf/default.nix
+++ home/commons/programs/nvf/default.nix
@@ -19,15 +19,47 @@
           name = "onedark";
         };
         statusline.lualine.enable = true;
+        tabline.nvimBufferline.enable = true;
+        autopairs.nvim-autopairs.enable = true;
         telescope.enable = true;
         autocomplete.nvim-cmp.enable = true;
+        utility.motion.hop.enable = true;
+        ui.colorizer.enable = true;
+        comments.comment-nvim.enable = true;
+        debugger.nvim-dap.enable = true;
+        utility.preview.glow.enable = true;
+        lsp.trouble.enable = true;
+        filetree.neo-tree.enable = true;
+        terminal.toggleterm.enable = true;
+
         languages = {
+          markdown.enable = true;
           enableLSP = true;
           enableTreesitter = true;
           nix.enable = true;
           ts.enable = true;
           rust.enable = true;
         };
+
+        binds.whichKey = {
+          enable = true;
+        };
+
+        keymaps = [
+          {
+            key = "<Space>";
+            mode = ["n" "v" "c"];
+            silent = true;
+            action = "<NOP>";
+          }
+          {
+            key = "<leader>s";
+            mode = "n";
+            silent = true;
+            action = ":w<CR>";
+            desc = "Save buffer";
+          }
+        ];
       };
     };
   };
