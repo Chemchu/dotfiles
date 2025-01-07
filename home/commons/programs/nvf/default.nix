ecb9fc1b553ebd4571318551cbc1958cf146d589
Chemchu <gustavo_9_rj@hotmail.com>
2025-01-07 20:08:07 +0100

    feat: nvf added and working

--- home/commons/programs/nvf/default.nix
+++ home/commons/programs/nvf/default.nix
@@ -9,30 +9,26 @@
 
   programs.nvf = {
     enable = true;
+
     settings = {
-      vim.viAlias = false;
-      vim.vimAlias = true;
-      vim.lsp = {
-        enable = true;
+      vim = {
+        viAlias = true;
+        vimAlias = true;
+        theme = {
+          enable = true;
+          name = "onedark";
+        };
+        statusline.lualine.enable = true;
+        telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+        languages = {
+          enableLSP = true;
+          enableTreesitter = true;
+          nix.enable = true;
+          ts.enable = true;
+          rust.enable = true;
+        };
       };
     };
   };
-
-  # nvf = {
-  #   enable = true;
-  #   theme = {
-  #     enable = true;
-  #     name = "onedark";
-  #   };
-  #   statusline.lualine.enable = true;
-  #   telescope.enable = true;
-  #   autocomplete.nvim-cmp.enable = true;
-  #   languages = {
-  #     enableLSP = true;
-  #     enableTreesitter = true;
-  #     nix.enable = true;
-  #     ts.enable = true;
-  #     rust.enable = true;
-  #   };
-  # };
 }
