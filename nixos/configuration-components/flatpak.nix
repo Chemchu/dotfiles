7e74cf8bcac9e9127480e67eda2c9bc331e9d8ed
Chemchu <gustavo_9_rj@hotmail.com>
2025-07-06 18:12:54 +0100

    feat: added filesystem access to zen

--- nixos/configuration-components/flatpak.nix
+++ nixos/configuration-components/flatpak.nix
@@ -1,9 +1,24 @@
 {
   # This is installed system-wide, so it's not necessary to install it in the user profile.
-  services.flatpak.enable = true;
-  services.flatpak.packages = [
-    #"io.dbeaver.DBeaverCommunity"
-    #"com.github.IsmaelMartinez.teams_for_linux"
-    "app.zen_browser.zen"
-  ];
+  services.flatpak = {
+    enable = true;
+    packages = [
+      #"io.dbeaver.DBeaverCommunity"
+      #"com.github.IsmaelMartinez.teams_for_linux"
+      "app.zen_browser.zen"
+    ];
+    overrides = {
+      global = {
+        Context.filesystems = [
+          "home"
+        ];
+      };
+      "app.zen_browser.zen".Context = {
+        filesystems = [
+          "home"
+          "host"
+        ];
+      };
+    };
+  };
 }
