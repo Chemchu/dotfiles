a483cfa41999551e8cfbb44df1ffbfaf17fdc8ae
Chemchu <gustavo_9_rj@hotmail.com>
2026-04-19 22:31:16 +0100

    fix: renamed niri config

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -52,6 +52,16 @@
         ];
 
         binds = {
+          # Niri
+          "Mod+F2".show-hotkey-overlay = {};
+          "Mod+M".quit = {};
+          "Mod+F".maximize-column = {};
+          "Mod+Shift+F".fullscreen-window = {};
+          "Mod+Minus".set-column-width = "-10%";
+          "Mod+Equal".set-column-width = "+10%";
+          "Mod+Shift+Minus".set-window-height = "-10%";
+          "Mod+Shift+Equal".set-window-height = "+10%";
+
           # Apps
           "Mod+Q".spawn-sh = lib.getExe pkgs.kitty;
           "Mod+W".spawn-sh = "zen";
@@ -76,30 +86,6 @@
           "Mod+Shift+L".move-column-right = {};
           "Mod+Shift+K".move-window-to-workspace-up = {};
           "Mod+Shift+J".move-window-to-workspace-down = {};
-
-          # Switch workspaces
-          "Mod+1".focus-workspace = 1;
-          "Mod+2".focus-workspace = 2;
-          "Mod+3".focus-workspace = 3;
-          "Mod+4".focus-workspace = 4;
-          "Mod+5".focus-workspace = 5;
-          "Mod+6".focus-workspace = 6;
-          "Mod+7".focus-workspace = 7;
-          "Mod+8".focus-workspace = 8;
-          "Mod+9".focus-workspace = 9;
-          "Mod+0".focus-workspace = 10;
-
-          # Move window to workspace
-          "Mod+Shift+1".move-window-to-workspace = 1;
-          "Mod+Shift+2".move-window-to-workspace = 2;
-          "Mod+Shift+3".move-window-to-workspace = 3;
-          "Mod+Shift+4".move-window-to-workspace = 4;
-          "Mod+Shift+5".move-window-to-workspace = 5;
-          "Mod+Shift+6".move-window-to-workspace = 6;
-          "Mod+Shift+7".move-window-to-workspace = 7;
-          "Mod+Shift+8".move-window-to-workspace = 8;
-          "Mod+Shift+9".move-window-to-workspace = 9;
-          "Mod+Shift+0".move-window-to-workspace = 10;
 
           # Volume
           "XF86AudioRaiseVolume".spawn-sh = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.15+ --limit 1";
