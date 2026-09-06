836b70e5338634ddbd67f534513bd015749eb1b4
Chemchu <gustavo_9_rj@hotmail.com>
2026-09-06 13:38:49 +0100

    feat: updated noctalia.json

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -1,14 +1,12 @@
-{
-  self,
-  inputs,
-  ...
-}: {
+{inputs, ...}: {
   perSystem = {pkgs, ...}: {
     packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
-      settings =
-        (builtins.fromJSON
-          (builtins.readFile ./noctalia.json)).settings;
+      inherit
+        ((builtins.fromJSON
+          (builtins.readFile ./noctalia.json)))
+        settings
+        ;
     };
   };
 }
